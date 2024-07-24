59c6b9cb37f291ea30c90e32f4561d2b1cf065c1
Pedro-Maria-Capgemini <pedro.maria@capgemini.com>
2024-07-24 10:26:17 +0100

    fixing the test eslin error 3.0

--- src/tests/home.spec.ts
+++ src/tests/home.spec.ts
@@ -33,7 +33,7 @@ test('should display the correct color square based on API response', async ({ p
 
   // Mock the API to return a specific color
   await page.route('**/ServiceTesting/rest/ColorPicker/GetColor/?int=25', async (route) => {
-    await route.fulfill({
+    void route.fulfill({
       status: 200,
       body: 'red',
     });
@@ -57,7 +57,7 @@ test('should display an error message when the API request fails', async ({ page
 
   // Mock the API to return an error
   await page.route('**/ServiceTesting/rest/ColorPicker/GetColor/?int=25', async (route) => {
-    await route.fulfill({
+    void route.fulfill({
       status: 500,
       body: 'Error fetching data', // Mock API to return an error message
     });
